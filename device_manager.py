09db63622eabc1ce00bcfbe8a2299c42300b6f25
Freshman21445 <abrahamtariku1997@gmail.com>
2026-09-08 01:10:31 +0300

    Refactor device manager to use Flask API

--- device_manager.py
+++ device_manager.py
@@ -1,58 +1,33 @@
-# device_manager.py
+from flask import Flask, jsonify, request
 import json
-import random
+
+app = Flask(__name__)
 
 devices = [
     {'id': '1', 'name': 'Device 1', 'status': 'infected', 'ip': '192.168.1.2'},
     {'id': '2', 'name': 'Device 2', 'status': 'infected', 'ip': '192.168.1.3'},
-    # Add more devices as needed
 ]
 
-def get_devices():
-    return json.dumps(devices)
+@app.route('/')
+def index():
+    return 'Device Manager API is running.'
 
-def send_command(device_id, command):
+@app.route('/devices', methods=['GET'])
+def get_devices():
+    return jsonify(devices)
+
+@app.route('/command', methods=['POST'])
+def send_command():
+    data = request.get_json()
+    if not data:
+        return jsonify({'error': 'Invalid JSON'}), 400
+    device_id = data.get('deviceId')
+    command = data.get('command')
     for device in devices:
         if device['id'] == device_id:
             if command == 'encrypt':
                 device['status'] = 'encrypting'
             elif command == 'beacon':
                 device['status'] = 'beaconing'
-            break
-    return json.dumps({'success': True})
-
-if __name__ == '__main__':
-    from http.server import SimpleHTTPRequestHandler, HTTPServer
-
-    class DeviceHandler(SimpleHTTPRequestHandler):
-        def do_GET(self):
-            if self.path == '/devices':
-                self.send_response(200)
-                self.send_header('Content-type', 'application/json')
-                self.end_headers()
-                self.wfile.write(get_devices().encode('utf-8'))
-            else:
-                return super().do_GET()
-
-        def do_POST(self):
-            if self.path == '/command':
-                content_length = int(self.headers['Content-Length'])
-                post_data = self.rfile.read(content_length)
-                data = json.loads(post_data)
-                device_id = data['deviceId']
-                command = data['command']
-                response = send_command(device_id, command)
-                self.send_response(200)
-                self.send_header('Content-type', 'application/json')
-                self.end_headers()
-                self.wfile.write(response.encode('utf-8'))
-            else:
-                return super().do_POST()
-
-    def run(server_class=HTTPServer, handler_class=DeviceHandler, port=8000):
-        server_address = ('', port)
-        httpd = server_class(server_address, handler_class)
-        print(f'Starting httpd server on port {port}')
-        httpd.serve_forever()
-
-    run()
+            return jsonify({'success': True})
+    return jsonify({'error': 'Device not found'}), 404
